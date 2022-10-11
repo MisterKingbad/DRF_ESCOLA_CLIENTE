972bac8cd588836266587eef4248e68e59e7bc18
MisterKingbad <davymichal20@gmail.com>
2022-10-11 16:55:08 -0300

    feat: :sparkles:

--- escola/views.py
+++ escola/views.py
@@ -1,13 +1,10 @@
 from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
 from escola.serializer import AlunoSerializer, AlunoSerializerV2, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer,ListaAlunoMatriculadosSerializer
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 
 class AlunosViewsSet(viewsets.ModelViewSet):
         """Exibindo todos os alunas e alunas"""
         queryset = Aluno.objects.all()
-        authentication_classes = [BasicAuthentication]
         def get_serializer_class(self):
                 if self.request.version == 'v2':
                         return AlunoSerializerV2
@@ -18,13 +15,11 @@ class CursosViewset(viewsets.ModelViewSet):
         """Ëxibindo todos os cursos"""
         queryset = Curso.objects.all()
         serializer_class = CursoSerializer
-        authentication_classes = [BasicAuthentication]
 
 class MatriculasViewsSet(viewsets.ModelViewSet):
         """Exibindo todas as matriculas"""
         queryset = Matricula.objects.all()
         serializer_class = MatriculaSerializer
-        authentication_classes = [BasicAuthentication]
 
 class ListaMatriculasAluno(generics.ListAPIView):
         """Listando as matriculas de um aluno(a)"""
@@ -32,7 +27,6 @@ class ListaMatriculasAluno(generics.ListAPIView):
                 queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
                 return queryset
         serializer_class = ListaMatriculasAlunoSerializer
-        authentication_classes = [BasicAuthentication]
 
 class ListaAlunosMatriculados(generics.ListAPIView):
         """Listando alunos(as) matriculados em um curso"""
@@ -40,5 +34,4 @@ class ListaAlunosMatriculados(generics.ListAPIView):
                 queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
                 return queryset
         serializer_class = ListaAlunoMatriculadosSerializer
-        authentication_classes = [BasicAuthentication]
         
--- setup/settings.py
+++ setup/settings.py
@@ -129,5 +129,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
         'rest_framework.permissions.DjangoModelPermissions',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',    
+    ],
 }
